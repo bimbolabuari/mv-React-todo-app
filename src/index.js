76bdbbce277af29e3f13695c9fb895a647fde2c5
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-17 18:20:53 +0100

    feat: Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import TodoContainer from './classBased/components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
